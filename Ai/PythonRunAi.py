3c2e046c393fa4933300faec41d87e80624631ac
Ahrirg <ldargevicius20@gmail.com>
2025-03-12 18:39:37 +0200

    send, return files, readme + file structure fixes

--- Ai/PythonRunAi.py
+++ Ai/PythonRunAi.py
@@ -2,18 +2,24 @@ import sys
 from llama_cpp import Llama
 
 llm = Llama(
-        model_path="./Ai/Model/DeepSeekTest.gguf",
+        model_path="./Ai/Model/Bollywood.gguf",
         n_gpu_layers=-1, # Uncomment to use GPU acceleration
-      # seed=1337, # Uncomment to set a specific seed
-        n_ctx=50048, # Uncomment to increase the context window
+        seed=1335, # Uncomment to set a specific seed
+        #n_ctx=512, # Uncomment to increase the context window
         # n_ctx = 131070,
         # verbose=False
 )
 
 output = llm(
-      "Q: Name the planets in the our solar system? A: ", # Prompt
+      """Create a fictional Bollywood song based on the provided theme. Your song should capture the essence of Bollywood music, incorporating catchy melodies, lively rhythms, and engaging lyrics.
+
+### Instructio:
+A neighbour who helped rescue horses from the property's stable said she was woken by an explosion and "looked out the window to see a sea of orange".
+
+### Response:
+""", # Prompt
       max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
-      stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
+      stop=["<|end_of_text|>", "\n"], # Stop generating just before the model would generate a new question
       echo=True # Echo the prompt back in the output
 ) # Generate a completion, can also call create_completion
 print(output['choices'][0]['text'])
--- public/index.js
+++ public/index.js
@@ -1,1 +1,36 @@
 alert("Test")
+const currentUrl = window.location.href;
+
+function SendJson() {
+    FileName = "Test.py"
+    FileLocation = "UploadedFiles"
+
+    data = {
+        "FileName": FileName,
+        "Location": FileLocation
+    }
+
+    fetch(currentUrl + "getFileData", {
+        method: 'POST',  // Use POST to send data in the body
+        headers: {
+          'Content-Type': 'application/json',  // Specify the content type as JSON
+        },
+        body: JSON.stringify(data),  // Convert the JavaScript object to a JSON string
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to download file');
+          }
+          return response.blob();  
+        })  // Parse the JSON response
+        .then(blob => {
+          // Create a link element to download the file
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(blob);  // Create a URL for the Blob
+          link.download = 'example.txt';  // The filename the user will see when downloading
+          link.click();  // Programmatically trigger a click to download the file
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+}
