c20cafc7a85c8a3c5c2dc75c58c387a942951ed4
ridas <ridas2005arstikaitis@gmail.com>
2025-03-14 00:41:06 +0200

    asAS

--- front_end/src/Home.jsx
+++ front_end/src/Home.jsx
@@ -1,18 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import NavBar from "./NavBar.jsx";
+import "./Home.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="home-container">
-      <h1 className="text-3xl font-bold">Make Your Data Visible!</h1>
-      <button 
-        onClick={() => navigate("/main")} 
-        className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
-      >
-        Get Started!
-      </button>
+    <div className="backgroundColor">
+        <NavBar></NavBar>
     </div>
+        
   );
 };
 
