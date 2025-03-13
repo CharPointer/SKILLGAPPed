34686ce13d90a25dbf3ba82be4fbf1195fe0436a
nothinDD <andriekusbenediktas@gmail.com>
2025-03-13 21:01:58 +0200

    YourFilesTab

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -2,20 +2,26 @@ import NavBar from "./NavBar.jsx";
 import ShowPlot from "./ShowPlot.jsx";
 import UploadFile from "./UploadFile.jsx";
 import React,{useEffect} from 'react';
-
+import YourFiles from './YourFiles.jsx';
 function App() {
   useEffect(()=>{
     document.title="skillGAPped";
   },[]);
 
   return(
-    <div className="App-background">
-          <NavBar/>
+    <>
+      <div className="App-background">
+        <NavBar/>
+        <YourFiles/>
+        <div>
           <ShowPlot/>
           <div className="UltraCoolButtons">
             <UploadFile/>
           </div>
-    </div>
+        </div>
+      </div>
+    </>
+
   );
   }
 
