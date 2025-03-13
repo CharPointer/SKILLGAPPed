b642ca255777ce6a0ae81ade9f55e935de729f1c
nothinDD <andriekusbenediktas@gmail.com>
2025-03-13 23:07:06 +0200

    UploadedFIles

--- front_end/src/YourFiles.jsx
+++ front_end/src/YourFiles.jsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from "react";
 
 function YourFiles() {
-
-    const [files, setFiles] = useState([]);
+    const [files, setFiles] = useState([]); 
 
     async function fetchData() {
         try {
-            const res = await (await fetch('http://localhost:3000/getFiledata')).json();
+            const res = await (await fetch('http://localhost:3000/getFilesNames')).json();
             let data = res.Files;
-            setFiles(prevFiles => [...prevFiles, ...data]); 
+            setFiles(prevFiles => Array.from(new Set([...prevFiles, ...data]))); 
         } catch (error) {
-            console.log("Failed to get files from API");
+            console.log("Failed to get file names from API");
         }
     }
 
     useEffect(() => {
-
-        fetchData();
-    });
+        fetchData(); 
+    }, []);
 
     return (
         <div className="YourFiles">
@@ -25,14 +23,13 @@ function YourFiles() {
             <hr />
             <div className="YourFiles-Container">
                 <div className="YourFiles-Buttons">
-                    
-                        {files.length > 0 && files.map((name) => (
-                        <button key={name} className="GetFileNames-btn">{name}</button>
-                        ))}
-
-                    <button className="GetFileNames-btn">Test.html</button>
-                    <button className="GetFileNames-btn">Test.html</button>
-                    <button className="GetFileNames-btn">Test.html</button>
+                    {files.length > 0 ? (
+                        files.map(name => (
+                            <button key={name} className="GetFileNames-btn">{name}</button>
+                        ))
+                    ) : (
+                        <p>Loading files...</p>
+                    )}
                 </div>
             </div>
         </div>
